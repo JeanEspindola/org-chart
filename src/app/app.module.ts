603d98f3455fa68f03c7c45129aa40b7fb592c05
JeanEspindola <jean.espindola@gmail.com>
2017-11-24 21:06:30 -0200

    Org-chart | Adding new components Team, Team-Detail, Member components. Switching default styling to less. Adding json response data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
+import { TeamDetailComponent } from './team-detail/team-detail.component';
+import { MemberComponent } from './member/member.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent,
+    TeamDetailComponent,
+    MemberComponent
   ],
   imports: [
     BrowserModule

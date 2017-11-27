afd3386c774699e3db32ec01b5cd1270b3b63563
JeanEspindola <jean.espindola@gmail.com>
2017-11-27 20:24:50 -0200

    Org-chart | Fixing unit testing.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,20 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { Observable } from 'rxjs/Rx';
 
 import { AppComponent } from './app.component';
 import { TeamComponent } from './team/team.component';
 import { TeamDetailComponent } from './team-detail/team-detail.component';
 import { MemberComponent } from './member/member.component';
 import { OrganizationService } from './organization.service';
+import { Organization } from './organization';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let organizationService: OrganizationService;
+  let app: AppComponent;
+  let organization: Organization;
+  let spy: any;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -17,24 +25,47 @@ describe('AppComponent', () => {
       ],
       providers: [OrganizationService]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    organizationService = fixture.debugElement.injector.get(OrganizationService);
+
+    organization = {
+      organization: 'Lobster',
+      location: 'Austin',
+      teams: [
+        {team: 'Tech', location: 'Amsterdam', picture: 'www.picture.com', members: []},
+        {team: 'Inc', location: 'Cape Town', picture: 'www.picture.com', members: []}
+      ]
+    };
   }));
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
+  it('should not call getOrganization Service before OnInit', () => {
+    spy = spyOn(organizationService, 'getOrganization').and.returnValue(Observable.of(organization));
+    expect(spy.calls.any()).toBe(false);
+  });
+
+  it('should call getOrganizationData', async(() => {
+    spy = spyOn(organizationService, 'getOrganization').and.returnValue(Observable.of(organization));
+
+    app.getOrganizationData();
+    fixture.detectChanges();
+    expect(spy.calls.any()).toBe(true, 'getOrganization called');
+    expect(app.organization === organization).toBeTruthy();
+  }));
+
   it(`should have as title 'Org Chart'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Organization Chart');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Organization Chart');
   }));
+
 });
